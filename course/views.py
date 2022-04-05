cdab264017e3e99d632b68f4c7c70f95aa0020af
Arman170616 <armanice09@gmail.com>
2022-04-05 18:10:41 +0600

    DTL Variable Coding

--- course/views.py
+++ course/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def learn_django(request):
-    return render(request, 'course/courseone.html', context={'nm':'Django'})
+    cname = 'Django'
+    duration = '4 months'
+    seats = 10
+    django_details = {'nm':cname, 'du': duration, 'st':seats}
+    return render(request, 'course/courseone.html', context=django_details)
